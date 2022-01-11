f78b1e58d7b6f30c1bbe493f92457dc82ea0b36e
Adrian JUNG <ajung@e2r3p11.clusters.42paris.fr>
2022-01-11 19:03:04 +0100

    push auto

--- src/print_map.c
+++ src/print_map.c
@@ -6,7 +6,7 @@
 /*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/09 20:58:38 by ajung             #+#    #+#             */
-/*   Updated: 2022/01/10 21:18:05 by ajung            ###   ########.fr       */
+/*   Updated: 2022/01/11 17:26:34 by ajung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ int	get_scale(t_all *all, int x, int y)
 	all->coor.scale = 1;
 	while (check)
 	{
-		if (((300 < x) && (x < 1920)) &&
-			((0 < y) && (y < 1080)))
+		if (((300 < x * all->coor.scale) && (x * all->coor.scale < 1920)) &&
+			((0 < y * all->coor.scale) && (y * all->coor.scale < 1080)))
 			check = 0;
 		else
 			all->coor.scale -= (all->coor.scale / 100);
@@ -36,4 +36,11 @@ int	get_scale(t_all *all, int x, int y)
 	return (0);
 }
 
-int find_corners
+int find_corners(t_all *all)
+{
+	all->coor.corner.x_corner1 = all->coor.x_origin;
+	all->coor.corner.y_corner1 = all->coor.y_origin;
+	all->coor.corner.x_corner2 = all ->coor.x_origin + 50 * all->map_data.size_line;
+	all->coor.corner.y_corner2 = all->coor.y_origin;
+	get_scale(all, all->coor.x_origin, all->coor.y_origin);
+}
