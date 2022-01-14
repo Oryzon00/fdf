c2735143f2af7bc13d069c8d7a2cf59a360d9296
Adrian Jung <adrian.mael.jung@gmail.com>
2022-01-14 15:30:26 +0100

    push auto

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 17:13:39 by ajung             #+#    #+#             */
-/*   Updated: 2022/01/13 20:45:07 by ajung            ###   ########.fr       */
+/*   Updated: 2022/01/14 15:18:14 by ajung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int main(int ac, char **av)
 {
 	t_all all;
+	char	*str= "Bonjour, allo, test";
 
 	if (ac != 2)
 		return (0);
@@ -26,11 +27,15 @@ int main(int ac, char **av)
 	print_map(&all);
 	hook_window(&all);
 	// init_origin(&all);
-	// all.coor.droite.xA = 201;
-	// all.coor.droite.yA = 0;
-	// all.coor.droite.xB = 201;
-	// all.coor.droite.yB = 1000;
+	// all.coor.droite.xA = 100;
+	// all.coor.droite.yA = 100;
+	// all.coor.droite.xB = 800;
+	// all.coor.droite.yB = 500;
 	// print_line(&all);
+	mlx_put_image_to_window(all.vars.mlx, all.vars.win,
+	all.image_data.img_ptr, 0, 0);
+	mlx_string_put(all.vars.mlx, all.vars.win, 200, 200, 0x00FFFFFF, str);
+
 	mlx_loop(all.vars.mlx);
 	return (0);
 }
--- src/window.c
+++ src/window.c
@@ -6,7 +6,7 @@
 /*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 23:21:20 by ajung             #+#    #+#             */
-/*   Updated: 2022/01/09 22:42:09 by ajung            ###   ########.fr       */
+/*   Updated: 2022/01/14 15:18:06 by ajung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	create_window(t_all *all)
 {
-	char	*str= "Bonjour, allo, test";
+	
 	
 	all->vars.mlx = mlx_init();
 	if (!all->vars.mlx)
@@ -24,8 +24,6 @@ int	create_window(t_all *all)
 	all->image_data.addr = mlx_get_data_addr(all->image_data.img_ptr,
 		&all->image_data.bits_per_pixel, &all->image_data.line_length, 
 		&all->image_data.endian);
-	mlx_put_image_to_window(all->vars.mlx, all->vars.win,
-		all->image_data.img_ptr, 0, 0);
-	mlx_string_put(all->vars.mlx, all->vars.win, 200, 200, 0x00FFFFFF, str);
+
 	return (0);
 }
